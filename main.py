f2732d4894e88266b6368f846a77dd9e780b5f0b
Ari Said <inf24228@lehre.dhbw-stuttgart.de>
2025-09-17 00:15:25 +0200

    Added conversion to brightness matrix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from mpmath.libmp import round_down
 
 img = Image.open("ascii-pineapple.jpg")
 print(img.mode, img.width, img.height, img.format)
@@ -11,4 +12,9 @@ for i in range(0, img.height):
     img_matrix.append(row)
 
 print(len(img_matrix))
+print(img_matrix[0][0])
+for i in range(0, len(img_matrix)):
+    for j in range(0, len(img_matrix)):
+        img_matrix[i][j] = int((img_matrix[i][j][0] + img_matrix[i][j][1] + img_matrix[i][j][2])/3)
 
+print(img_matrix[0][0])
