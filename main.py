a17d2289f59bc9ab89610ed4fbddb2405a89ebc8
Ari510 <ari510dev@gmail.com>
2025-10-13 15:57:12 +0200

    minor optimizations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ img = Image.open(path)
 max_size = (500, 500)  # fits inside this box
 img.thumbnail(max_size, Image.LANCZOS)
 
+characters = list("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
 print(img.width, img.height, img.format)
 
 img_matrix = []
@@ -20,26 +21,17 @@ print(img_matrix[0][0])
 for i in range(0, len(img_matrix)):
     for j in range(0, len(img_matrix[0])):
         img_matrix[i][j] = (img_matrix[i][j][0] + img_matrix[i][j][1] + img_matrix[i][j][2])/3
-
-print(img_matrix[0][0])
+        if img_matrix[i][j] == 255:
+            img_matrix[i][j] = 254
+        img_matrix[i][j] = characters[int(img_matrix[i][j] // 3.923)]
+        print(img_matrix[i][j], sep="", end="")
+    print()
 
 # We are using these 65 ASCII characters: `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$
 # 255/65 = 3,923...
 
-characters = list("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
-print(characters)
-print(len(characters))
 
 # I was originally going to make the brightness values into integers but keeping them as floats
 # will make this easier/better
 
-for i in range(0, len(img_matrix)):
-    for j in range(0, len(img_matrix[0])):
-        if img_matrix[i][j] == 255:
-            img_matrix[i][j] = 254
-        img_matrix[i][j] = characters[int(img_matrix[i][j] // 3.923)]
 
-for i in range(0, len(img_matrix)):
-    for j in range(0, len(img_matrix[0])):
-        print(img_matrix[i][j],img_matrix[i][j], sep="", end="")
-    print()
